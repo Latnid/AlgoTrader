0e62ea5b09df412763fa50d34845ab5d69a83bea
Dintal <dintalsky@gmail.com>
2022-11-04 05:44:08 -0400

    Base

---
+++ Modules/Authorization.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import alpaca_trade_api as tradeapi
+import pandas as pd
+
+# Load the required config from .env file under root directory.
+load_dotenv()
+
+# acquire all info and assign variables.
+ALPACA_API_KEY = os.getenv('ALPACA_API_KEY')
+ALPACA_SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
+
+#Function for paper trading api or data only api
+def api_access(type):
+    if type == 'paper':
+        try:
+            api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, 'https://paper-api.alpaca.markets')
+            account = api.get_account()
+            status = account.status
+            print(f"Paper API connected")
+            return api
+        except:
+            raise Exception("Please configur the .env file under root directory")
+    elif type == 'data':
+        try:
+            api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, 'https://data.alpaca.markets')
+            start_date = pd.Timestamp("2020-06-01", tz="America/New_York").isoformat()
+            end_date = pd.Timestamp("2020-06-05", tz="America/New_York").isoformat()
+            api.get_bars("AAPL",timeframe = "1Day",start = start_date,end = end_date)
+            print('Data API connected')
+            return api
+        except:
+            raise Exception("Please configur the .env file under root directory")
---
+++ Modules/CleanData.py
@@ -0,0 +1,21 @@
+from Authorization import api_access
+import pandas as pd
+import numpy as np
+import hvplot.pandas
+
+
+def get_data(type,Symbol,timeframe,start = None,end = None):
+    # get api access
+    paper_api = api_access(type)
+    
+    # Set timezone as required
+    if start != None:            
+        start = pd.Timestamp(start, tz="America/New_York").isoformat()
+    if end != None:
+        end = pd.Timestamp(end, tz="America/New_York").isoformat()
+    # Request data.
+    data_df = paper_api.get_bars(Symbol,timeframe,start,end).df
+    #Convert received data time zone the same as WallStreet
+    data_df.index = data_df.index.tz_convert('America/New_York')
+    print(f'{Symbol} data was retrieved successfully')
+    return data_df
